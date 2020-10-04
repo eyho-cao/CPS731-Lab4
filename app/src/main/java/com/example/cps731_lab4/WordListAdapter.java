80cd6405c49ebbd89b2b7f45c33c2f230b103420
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2020-10-04 17:28:49 -0400

    Implemented OnClick action(change page to recipe page and display information)  -images not working yet(Permission denied (missing INTERNET permission))

--- app/src/main/java/com/example/cps731_lab4/WordListAdapter.java
+++ app/src/main/java/com/example/cps731_lab4/WordListAdapter.java
@@ -1,38 +1,30 @@
 package com.example.cps731_lab4;
 
 import android.content.Context;
-import android.content.Intent;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
-    private final ArrayList<String> urlList;
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder>  {
+    private final LinkedList<String> mWordList;
     private LayoutInflater mInflater;
-    private  ArrayList<String> mWordList;
     private String recipeDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin porttitor fermentum. Suspendisse id magna finibus, molestie felis aliquet, pellentesque diam. Nullam elit justo, lobortis id lectus nec, dictum iaculis felis.";
-    private Context mContext;
-    private int posit;
-    public WordListAdapter(Context context, ArrayList<String> wordList, ArrayList<String> urlList) {
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
 
         mInflater = LayoutInflater.from(context);
         this.mWordList = wordList;
-        this.urlList = urlList;
-        mContext = context;
     }
 
     @NonNull
     @Override
-    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View mItemView = mInflater.inflate(R.layout.wordlist_item,
                 parent, false);
         return new WordViewHolder(mItemView, this);
@@ -42,7 +34,6 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
     public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
         String mCurrent = mWordList.get(position);
         holder.wordItemView.setText(Html.fromHtml("<h4>" +mCurrent +"</h4>"+"<small>" +recipeDesc +"</small>"));
-        posit = position;
     }
 
     @Override
@@ -67,14 +58,6 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
 
         @Override
         public void onClick(View view) {
-            int mPosition = getLayoutPosition();
-            Toast.makeText(mContext, Integer.toString(mPosition), Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(mContext, RecipePage.class);
-            intent.putExtra("image_url", urlList.get(mPosition));
-            intent.putExtra("recipe_name",mWordList.get(mPosition));
-            intent.putExtra("recipe_desc", recipeDesc);
-            mContext.startActivity(intent);
         }
     }
-
 }
